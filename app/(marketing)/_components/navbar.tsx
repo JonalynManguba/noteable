b4c4414a77be7e14919bec9389398459057468e6
Hirayamanawari <jonalynmanguba@gmail.com>
2024-02-01 17:39:10 +0800

    few updates in navbar

---
+++ app/(marketing)/_components/navbar.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+
+
+
+export const Navbar = () => {
+    console.log("Navbar is working");
+    
+    return (
+        <div className="text-black"> 
+            Navbar!
+        </div>
+    )
+
+
+}
+
