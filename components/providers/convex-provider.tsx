2a06cfce019053384c71968f161fc0a11ac1c083
Hirayamanawari <jonalynmanguba@gmail.com>
2024-02-02 10:53:01 +0800

    fixing minor issue

--- components/providers/convex-provider.tsx
+++ components/providers/convex-provider.tsx
@@ -4,20 +4,17 @@
 // next step go to 
 "use client";
 
-
-import React from "react";
-import {ConvexReactClient} from "convex/react";
-import { ConvexProviderWithClerk} from "convex/react-clerk";
+import { ReactNode } from "react";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ClerkProvider, useAuth } from "@clerk/clerk-react";
 
-const convex = new ConvexReactClient (process.env.NEXT_PUBLIC_CONVEX_URL!)
-
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export const ConvexClientProvider = ({
-    children 
+  children
 }: {
-    
-    children: ReactNode;
+  children: ReactNode;
 }) => {
   return (
     <ClerkProvider
